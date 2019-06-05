4ab6185cc472cd02a3e335a0e2b1290f34be8cb8
tianxiaoMCU <40310665+tianxiaoMCU@users.noreply.github.com>
2019-06-05 19:51:01 +0800

    auto find the output hex/bin file for keil

--- autoversionname.py
+++ autoversionname.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import xml.etree.ElementTree as ET
 import re
 
 fireware_version = ''
@@ -57,8 +58,58 @@ else:
 
 print(fireware_version)
 
+for entry in os.scandir():
+    if entry.is_file():
+        if entry.name.endswith('.eww'):
+            sys.exit(0)
+
+        elif entry.name.endswith('.uvproj') or entry.name.endswith('.uvprojx'):
+
+            if entry.name.endswith('.uvproj'):
+                uvoptfile = entry.name.replace('.uvproj', '.uvopt')
+            elif entry.name.endswith('.uvprojx'):
+                uvoptfile = entry.name.replace('.uvprojx', '.uvoptx')
+
+            tree = ET.ElementTree(file=uvoptfile)
+
+            # find current target
+            for tag in tree.findall('Target'):
+                TargetOption = tag.find('TargetOption')
+                OPTFL = TargetOption.find('OPTFL')
+                IsCurrentTarget = int(OPTFL.find('IsCurrentTarget').text)
+                if IsCurrentTarget:
+                    TargetName = tag.find('TargetName').text
+                    break
+
+            # get output directory and outputname
+            # ide is keil5
+            tree = ET.ElementTree(file=entry.name)
+            for tag in tree.find('Targets').findall('Target'):
+                if tag.find('TargetName').text == TargetName:
+                    TargetOption = tag.find('TargetOption')
+                    TargetCommonOption = TargetOption.find(
+                        'TargetCommonOption')
+                    OutputDirectory = TargetCommonOption.find(
+                        'OutputDirectory').text
+                    OutputDirectory = os.path.normpath(
+                        os.path.join(os.getcwd(), OutputDirectory))
+                    OutputName = TargetCommonOption.find(
+                        'OutputName').text
+
+            break
+
 # 将IDE生成的bin或者hex文件重命名为得到的版本号
-fireware_path = r"D:\IPoverUSB\keil\Objects\IPoverUSB.hex"
+fireware_hex_path = os.path.join(OutputDirectory, OutputName + '.hex')
+fireware_bin_path = os.path.join(OutputDirectory, OutputName + '.bin')
+
+if os.path.exists(fireware_hex_path):
+    fireware_path = fireware_hex_path
+elif os.path.exists(fireware_bin_path):
+    fireware_path = fireware_bin_path
+else:
+    print('No hex or bin file')
+    sys.exit(0)
+
 new_fireware_path = fireware_path.replace(
     os.path.basename(fireware_path), fireware_version + os.path.splitext(os.path.split(fireware_path)[1])[1])
 
